3df1391b2df37f84d4f43ed1c5626bf32df15799
shawn <echun1234@gmail.com>
2020-12-04 01:46:47 +0900

    이미지와 설명 매핑, 해쉬태그 입력창 생성

--- photofolio/lib/pages/edit_page.dart
+++ photofolio/lib/pages/edit_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_image_picker/flutter_web_image_picker.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:textfield_tags/textfield_tags.dart';
 
 class EditPage extends StatefulWidget{
   @override
@@ -16,18 +17,20 @@ class EditPageState extends State<EditPage>{
   Image currentImage;
   
   List<Image> images = List<Image>();
-  Map<String, Image> maps = Map<String, Image>(); //이미지라밸이 key
+  Map<String, Image> imagesMap = Map<String, Image>(); //이미지라밸이 key
 
-  Map<String,String> comments = Map<String,String>(); //이미지라밸이 key
+  List<String> comments = List<String>();
+  Map<String,String> commentsMap = Map<String,String>(); //이미지라밸이 key
 
   TextEditingController commentController = TextEditingController(); 
 
+  List<String> tags = List<String>();
   // @override
   // void initState() {
   //   ms['default'] = image;
     
   // }
-
+  int i =0;
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +48,7 @@ class EditPageState extends State<EditPage>{
               //print(_image.toString());
               currentImage = _image;
               images.add(_image);
-              maps[_image.semanticLabel] = _image;
+              imagesMap[_image.semanticLabel] = _image;          
             });
           },
         ),
@@ -54,6 +57,7 @@ class EditPageState extends State<EditPage>{
             Flexible(
               flex: 1,
               child: Swiper(
+                // layout: SwiperLayout.STACK,
                 itemCount: images.length,
                 itemBuilder: (context,count){
                   // print(count.toString() + "vvvvvvvvvvvv");
@@ -68,22 +72,72 @@ class EditPageState extends State<EditPage>{
                   disableColor: Colors.green,
                 ),
                 loop: false,
+                onTap: (int index){
+                  print("onTap : "+index.toString());
+                },
                 onIndexChanged: (int index) {
-                  
-                  commentController.text = comments[currentImage.semanticLabel];
-                  currentImage = images[index];
-                  print('sdafsfdaf\t'+index.toString()+'\t' + currentImage.semanticLabel);
-                  comments[currentImage.semanticLabel] = commentController.text;
-                  print(comments.toString());
-                  //commentController.clear();
-                  
-                  
+
+                  currentImage = images[i];
+
+                  commentsMap[currentImage.semanticLabel] = commentController.text;
+
+      
+                  commentController.clear();
+
+                  commentController.text = commentsMap[images[index].semanticLabel];
+                  i=index;
                 },
               ),
             ),
             Flexible(
               child: TextField(controller: commentController,),
               
+            ),
+            Flexible(
+              flex: 1,
+              child: TextFieldTags(
+                textFieldStyler: TextFieldStyler(
+                  hintText: "Vz",
+                  helperText: "엔터누르삼",
+                  cursorColor: Colors.orangeAccent,
+                  // textFieldFocusedBorder: ,
+                  textFieldBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.zero,
+                    borderSide: BorderSide(color: Colors.purple)
+                  ),
+                  textFieldFocusedBorder: OutlineInputBorder(
+                    gapPadding: 10,
+                    borderSide: BorderSide(color: Colors.purple)
+                  ),
+                  textFieldFilledColor: Colors.red,
+                  textFieldFilled: true
+                ),
+                tagsStyler: TagsStyler(
+                  tagTextStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                  tagDecoration: BoxDecoration(
+                    color: Colors.deepPurpleAccent[700],
+                    borderRadius: BorderRadius.circular(7)
+                  ),
+                  tagCancelIcon: Icon(
+                    Icons.cancel,
+                    color: Colors.white,
+                  )
+                ),
+                onTag: (tag) {
+                  //This give you tags entered
+                  print('onTag ' + tag);
+                  tags.add(tag);
+                  print(tags.toString());
+                },
+                onDelete: (tag) {
+                  print('onDelete ' + tag);
+                  tags.remove(tag);
+                  print(tags.toString());
+                },
+
+              )
             ),
             Flexible(
               child: RaisedButton(

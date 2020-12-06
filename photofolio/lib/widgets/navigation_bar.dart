52d71db7cdde10b5a3473dd9e1b43ef92f50375e
shawn <echun1234@gmail.com>
2020-12-07 01:11:37 +0900

    add login btn in navigator

--- photofolio/lib/widgets/navigation_bar.dart
+++ photofolio/lib/widgets/navigation_bar.dart
@@ -1,6 +1,9 @@
 
+import 'package:flutter/material.dart';
 import 'package:photofolio/routes/routes.dart';
 import 'package:flutter/cupertino.dart';
+import '../pages/login_page.dart';
+import '../routes/routes.dart';
 import 'navigation_item.dart';
 
 class NavigationBar extends StatefulWidget{
@@ -34,11 +37,59 @@ class NavigationBarState extends State<NavigationBar>{
             selected:index==1,
             onHighlight:onHighlight,
           ),
+          RaisedButton(
+            child:Text('dfgsf'),
+            color: Colors.greenAccent,
+            
+            onPressed: (){
+              
+              showLoginDialog(navKey.currentContext);
+            },
+          )
         ],
+        
       )
     );
     
   }
+
+  Future<void> showLoginDialog(BuildContext context){
+    return showGeneralDialog(
+      context: context,
+      barrierColor: Colors.blue.withAlpha(70),
+      transitionDuration: new Duration(milliseconds: 400),
+      pageBuilder: (BuildContext con, Animation ani, Animation secAni){
+        return AlertDialog(
+          elevation: 10,
+          content: Container(
+            width: MediaQuery.of(context).size.width*0.3,
+            child: SingleChildScrollView(
+            padding: EdgeInsets.zero,
+            //width: MediaQuery.of(context).size.width*0.4,
+            child: Column(
+              children: [
+                Align(
+                  child: IconButton(
+                      iconSize: 20,
+                      icon: Icon(Icons.cancel_outlined,),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        
+                      },
+                  ), 
+                  alignment: FractionalOffset(1, 0),
+                ),
+                LoginPage()
+              ],
+            ),
+          ),
+          )
+        );
+      }
+
+    );
+  }
+
 
   onHighlight(String route){
     switch (route){

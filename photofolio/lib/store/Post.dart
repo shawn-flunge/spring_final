d3a42ae10f63f26f6b8deebf104da504bb2922f4
platina78 <ghdtjdwns87@gmail.com>
2020-12-10 01:07:07 +0900

    Add Type Uint8List

--- photofolio/lib/store/Post.dart
+++ photofolio/lib/store/Post.dart
@@ -1,18 +1,30 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:typed_data';
+import 'dart:convert';
 
 class Post{
-  Image thumbNail;
+  Uint8List thumbNail;
   String title;
   String content;
+  List<Uint8List> postImages;
 
-
-  Post({this.title,this.content,this.thumbNail});
+  Post({this.title,this.content,this.thumbNail,this.postImages});
 
   factory Post.fromJson(Map<String,dynamic> json){
     return Post(
-      title:json['thumbNail'],
+      title:json['title'],
       content:json['password'],
       thumbNail:json['thumbNail'],
+      postImages:json['postImages'],
     );
+  }
+
+  String toJson(){
+    final Map<String,dynamic> data=new Map<String,dynamic>();
+    data['title']=this.title;
+    data['content']=this.content;
+    data['thumbNail']=this.thumbNail;
+    data['postImages']=this.postImages;
+    var jsonData=json.encode(data);
+    return jsonData;
   }
 }
